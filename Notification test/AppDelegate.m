e26fff59e91ccc15e97302057221f6aa08c7b4c8
kyle kersey <kylelk@comcast.net>
2013-11-02 17:07:37 -0700

    Initial Commit

---
+++ Notification test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Notification test
+//
+//  Created by Kyle on 11/2/13.
+//  Copyright (c) 2013 Kyle kersey. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
