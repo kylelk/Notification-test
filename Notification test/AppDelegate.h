d6d552959f2afd921f3fbe6bbb55200efa0cefb6
kyle kersey <kylelk@comcast.net>
2014-01-11 11:15:04 -0800

    Fixed getting the user reply

--- Notification test/AppDelegate.h
+++ Notification test/AppDelegate.h
@@ -15,6 +15,23 @@
 @property (weak) IBOutlet NSTextField *nMessage;
 -(IBAction)showNotification:(id)sender;
 
+@property (weak) IBOutlet NSTextField *nResponse;
+
 @property (assign) IBOutlet NSWindow *window;
+
+// Set to YES if the notification has a reply button. The default value is NO.
+// If both this and hasActionButton are YES, the reply button will be shown.
+@property BOOL hasReplyButton NS_AVAILABLE(10_9, NA);
+
+// Optional placeholder for inline reply field.
+@property (copy) NSString *responsePlaceholder NS_AVAILABLE(10_9, NA);
+
+// When a notification has been responded to, the NSUserNotificationCenter delegate
+// didActivateNotification: will be called with the notification with the activationType
+// set to NSUserNotificationActivationTypeReplied and the response set on the response property
+@property (readonly) NSAttributedString *response NS_AVAILABLE(10_9, NA);
+
+
+@property (copy) NSImage *contentImage NS_AVAILABLE(10_9, NA);
 
 @end
--- Notification test/AppDelegate.m
+++ Notification test/AppDelegate.m
@@ -11,23 +11,40 @@
 @implementation AppDelegate
 @synthesize nTitle;
 @synthesize nMessage;
+@synthesize nResponse;
+@synthesize window;
 
 - (IBAction)showNotification:(id)sender{
     NSUserNotification *notification = [[NSUserNotification alloc] init];
     notification.title = [nTitle stringValue];
     notification.informativeText = [nMessage stringValue];
-    notification.soundName = NSUserNotificationDefaultSoundName;
+    //notification.soundName = NSUserNotificationDefaultSoundName;
+    notification.responsePlaceholder = @"Reply";
+    notification.hasReplyButton = true;
+    notification.contentImage = [NSImage imageNamed:@"kyle.jpg"];
     
     [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
 }
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification;
+{
+        return YES;
+    
+}
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center
+       didActivateNotification:(NSUserNotification *)notification
+{
+    NSString* userResponse = notification.response.string;
+    [nResponse setStringValue:userResponse]; //set lable to responce
+}
+
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
-}
-- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification{
-    return YES;
+    //[[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+    NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
+    center.delegate = self;
+
 }
 
 @end
